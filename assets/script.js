6b5a0593e95bff76c6d1205be02c22939e9435ab
shane davis <120618274+Basil-NB@users.noreply.github.com>
2023-02-27 16:08:26 -0500

    added function saving items to local storage

--- assets/script.js
+++ assets/script.js
@@ -3,30 +3,28 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 var today = dayjs();
-var saveButtons = document.querySelectorAll(".saveBtn");
+
 var userImput = localStorage.getItem("Imput");
 
 
-$(function saveImput() {
-  saveButtons.addEventListener("click", function() {
-    if () {
-      .textContent = ;
-      localStorage.setItem("", );
-    }
-  });
+$(".saveBtn").on("click", function(){
+var time = $(this).attr("id")
+var text = $(this).siblings("textarea").val()
+localStorage.setItem(time, text)
+}) 
 
-    
-  }
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-});
 
-$(function classSelector() {
+$(function curentTime(hour) {
+  if (moment().isAfter(hour)) {
+    return 'future';
+
+} else if (moment().isBefore(hour)) {
+    return 'past';
+
+} else  {
+    return 'present';
+}
+
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
@@ -41,6 +39,6 @@ $(function () {
 
 });
 
-$(function () {
+$(function displayTime() {
 $('#currentDay').text(today.format('dddd, MMMM D'));
 });
