444b051f581cb3cf6c6747917e4e794079e21ef8
shane davis <120618274+Basil-NB@users.noreply.github.com>
2023-02-27 15:20:46 -0500

    added some variables to the first function

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,21 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var saveBtnEl = $('#save');
+var today = dayjs();
+var saveButtons = document.querySelectorAll(".saveBtn");
+var userImput = localStorage.getItem("Imput");
 
-$(function () {
+
+$(function saveImput() {
+  saveButtons.addEventListener("click", function() {
+    if () {
+      .textContent = ;
+      localStorage.setItem("", );
+    }
+  });
+
+    
+  }
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -14,7 +26,7 @@ $(function () {
   //
 });
 
-$(function () {
+$(function classSelector() {
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
@@ -30,6 +42,5 @@ $(function () {
 });
 
 $(function () {
-var today = dayjs();
 $('#currentDay').text(today.format('dddd, MMMM D'));
 });
